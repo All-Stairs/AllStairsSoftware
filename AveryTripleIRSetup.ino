677f7f361e1fe85d090230b7d8ea3779ff149693
Fernando-Esquivel <31659656+Fernando-Esquivel@users.noreply.github.com>
2017-11-13 17:05:21 -0600

    Add files via upload
    These are the latest relevant files for our project; My code for the multiplexer testing is here, as well as Avery's triple IR NeoPixel code is here; finally, Conley's most recent audio testing code (as of November 13 2017) is here

---
+++ AveryTripleIRSetup.ino
@@ -0,0 +1,102 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
+
+#define PIN 6
+#define irLedPin 4          // IR Led on this pin
+#define irSensorPin1 11       // IR sensor on this pin
+#define irSensorPin2 10
+#define irSensorPin3 9
+
+int irRead(int readPin, int triggerPin); //function prototype
+int SensorValue1;
+int SensorValue2;
+int SensorValue3;
+int steps[30];
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+  #if defined (__AVR_ATtiny85__)
+    if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+  #endif
+  // End of trinket special code
+
+  pinMode(irSensorPin1, INPUT);
+  pinMode(irSensorPin2, INPUT);
+  pinMode(irSensorPin3, INPUT);
+  pinMode(irLedPin, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("Program Starting");
+  delay(100); 
+
+  strip.begin();
+  strip.setBrightness(30);
+  strip.show(); // Initialize all pixels to 'off'
+
+  for(int i = 0; i < 30; i++){
+    steps[i] = 0;
+    Serial.println(steps[i]);
+  }
+}
+
+void loop() {
+  SensorValue1 = irRead(irSensorPin1, irLedPin);
+  SensorValue2 = irRead(irSensorPin2, irLedPin);
+  SensorValue3 = irRead(irSensorPin3, irLedPin);
+  if(SensorValue1 == 1){
+    steps[3] = 1;
+    Serial.println("RED");
+  }
+  if(SensorValue2 == 1){
+    steps[4] = 1;
+    Serial.println("GREEN");
+  }
+  if(SensorValue3 == 1){
+    steps[5] = 1;
+    Serial.println("BLUE");
+  }
+
+  delay(20);
+  lightTriggered2();
+}
+
+void lightTriggered2()
+{
+  for(int i = 0; i < 30; i++){
+    if(steps[i] == 1){
+      strip.setPixelColor(i, 0, 255, 0);
+      steps[i]++;
+    } else if(steps[i] == 50){
+      steps[i] = 0;
+      strip.setPixelColor(i, 0, 0, 0);
+    } else if(steps[i] > 1 && steps[i] < 50){
+      steps[i]++;
+    }
+  }
+  strip.show();
+}
+
+/******************************************************************************
+ * This function can be used with a panasonic pna4602m ir sensor
+ * it returns a zero if something is detected by the sensor, and a 1 otherwise
+ * The function bit bangs a 38.5khZ waveform to an IR led connected to the
+ * triggerPin for 1 millisecond, and then reads the IR sensor pin to see if
+ * the reflected IR has been detected
+ ******************************************************************************/
+int irRead(int readPin, int triggerPin)
+{
+  int halfPeriod = 13; //one period at 38.5khZ is aproximately 26 microseconds
+  int cycles = 38; //26 microseconds * 38 is more or less 1 millisecond
+  int i;
+  for (i=0; i <=cycles; i++)
+  {
+    digitalWrite(triggerPin, HIGH); 
+    delayMicroseconds(halfPeriod);
+    digitalWrite(triggerPin, LOW); 
+    delayMicroseconds(halfPeriod - 1);     // - 1 to make up for digitaWrite overhead    
+  }
+  return digitalRead(readPin);
+}
---
+++ _74hc165_practice.ino
@@ -0,0 +1,79 @@
+//    74HC595                           74HC165
+           
+             // Pin 1 -  LED 1                    Pin 1 -  Load
+             // Pin 2 -  LED 2                    Pin 2 -  Clock
+             // Pin 3 -  LED 3                    Pin 3 -  Input 4
+             // Pin 4 -  LED 4                    Pin 4 -  Input 5
+             // Pin 5 -  LED 5                    Pin 5 -  Input 6
+             // Pin 6 -  LED 6                    Pin 6 -  Input 7
+             // Pin 7 -  LED 7                    Pin 7 -  Data
+             // Pin 8 -  Negative (Ground)        Pin 8 -  Negative (Ground)
+             // Pin 9 -  (Not Used) Serial out    Pin 9 -  (Not Used)
+             // Pin 10 - MR Master Reset (+)      Pin 10 - (Not Used)
+             // Pin 11 - SHCP CLOCK               Pin 11 - Input 0
+             // Pin 12 - STCP LATCH               Pin 12 - Input 1
+             // Pin 13 - OE Output Enable (-)     Pin 13 - Input 2
+             // Pin 14 - DS DATA                  Pin 14 - Input 3
+             // Pin 15 - LED 0                    Pin 15 - Clock Enable (CE)
+             // Pin 16 - Voltage IN (+)           Pin 16 - Vcc Voltage IN (+)
+
+      int load            = 7;  // Connects to Load pin 1           74HC165
+      int clockEnablePin  = 4;  // Connects to Clock Enable pin 15  74HC165
+      int dataIn          = 5;  // Connects to the Data pin 7       74HC165
+      int clockIn         = 6;  // Connects to the Clock pin 2      74HC165
+   
+    void setup()
+    {
+        Serial.begin(9600);
+
+        
+        pinMode(load, OUTPUT);
+        pinMode(clockEnablePin, OUTPUT);
+        pinMode(clockIn, OUTPUT);
+        pinMode(dataIn, INPUT);
+    }
+    
+    void loop()
+    {
+      
+      digitalWrite(load,LOW);
+      delayMicroseconds(5);
+      digitalWrite(load,HIGH);
+      delayMicroseconds(5);
+     
+      digitalWrite(clockIn,HIGH);
+      digitalWrite(clockEnablePin,LOW);
+      byte incoming = shiftIn(dataIn, clockIn, LSBFIRST);
+      digitalWrite(clockEnablePin,HIGH);
+            
+      Serial.print("Pin States:\r\n");
+      Serial.println(incoming, BIN);
+      
+      //Printing individual bits of incoming byte
+      Serial.print("DIP 1:\r\n");
+      Serial.print(bitRead(incoming,7));
+      Serial.print("\r\n");      
+      Serial.print("DIP 2:\r\n");
+      Serial.print(bitRead(incoming,6));
+      Serial.print("\r\n");      
+      Serial.print("DIP 3:\r\n");
+      Serial.print(bitRead(incoming,5));
+      Serial.print("\r\n");      
+      Serial.print("DIP 4:\r\n");
+      Serial.print(bitRead(incoming,4));
+      Serial.print("\r\n");      
+      Serial.print("DIP 5:\r\n");
+      Serial.print(bitRead(incoming,3));
+      Serial.print("\r\n");      
+      Serial.print("DIP 6:\r\n");
+      Serial.print(bitRead(incoming,2));
+      Serial.print("\r\n");      
+      Serial.print("DIP 7:\r\n");
+      Serial.print(bitRead(incoming,1));
+      Serial.print("\r\n");      
+      Serial.print("DIP 8:\r\n");
+      Serial.print(bitRead(incoming,0));
+      Serial.print("\r\n");      
+      
+      delay(500);//slowing down for testing purposes
+      }
